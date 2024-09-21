0093812180e7b209f3dae00932bfe15d73107468
marcpaget <marc.d.paget@gmail.com>
2024-09-21 03:29:13 +0200

    virker stadig ikke

--- pages/spiring.vue
+++ pages/spiring.vue
@@ -1,24 +1,25 @@
 <template>
-    <div class="overflow-x-auto content-center">
-        <table class="table table-zebra">
-            <thead>
-                <tr>
-                    <th>Navn</th>
-                    <th>Min. spiretemp</th>
-                    <th>Max. spiretemp</th>
-                </tr>
-            </thead>
-            <tbody>
-                <div v-for="planter in frospiring" :key="planter.id">
-                    <tr>
-                        <td>{{ planter.planteart }}</td>
-                        <td>{{ planter.min_spire_temp }}</td>
-                        <td>{{ planter.max_spire_temp }}</td>
-                    </tr>
-                </div>
-            </tbody>
-        </table>
-    </div>
+  <div class="overflow-x-auto content-center">
+    <table class="table table-zebra">
+      <thead>
+        <tr>
+          <th>Navn</th>
+          <th>Min. spiretemp</th>
+          <th>Max. spiretemp</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="planter in frospiring"
+          :key="planter.id"
+        >
+          <td>{{ planter.planteart }}</td>
+          <td>{{ planter.min_spire_temp }}</td>
+          <td>{{ planter.max_spire_temp }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <!-- <script setup>
 const supabase = useSupabaseClient()
